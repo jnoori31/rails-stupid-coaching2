d6bca46b7b9a669e87173cb74c372715ad8b3d2e
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-09-20 11:19:16 +0100

    controller working with view response

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,6 +10,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-
+    @question = params[:query]
+    # name parameter in form = query which is the params
+    if @question.blank?
+      @answer = "I cant hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+        @answer = "Silly question, get dressed and go to work!"
+      else
+        @answer = "I dont care, get dressed and go to work!"
+    end
   end
 end
